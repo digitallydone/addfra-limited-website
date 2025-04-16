f4728bcb089820b79d73c0e40198e8d2f2e339e7
Mansir <mansiraziz93@gmail.com>
2025-04-16 00:45:04 +0000

    Update page.jsx

--- app/admin/products/page.jsx
+++ app/admin/products/page.jsx
@@ -311,16 +311,16 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
-import { getProducts } from "@/app/actions/product";
+// import { getProducts } from "@/app/actions/product";
 
 export default function ProductsPage() {
   // State for filters and pagination
-  // const [filters, setFilters] = useState({
-  //   category: "all",
-  //   status: "all",
-  //   search: "",
-  //   sort: "newest",
-  // });
+  const [filters, setFilters] = useState({
+    category: "all",
+    status: "all",
+    search: "",
+    sort: "newest",
+  });
   const [pagination, setPagination] = useState({
     page: 1,
     limit: 10,
