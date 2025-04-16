5d9a4ef24d00ab0d9d469bbe2706d3c9e9807f1e
Mansir <mansiraziz93@gmail.com>
2025-04-16 00:42:39 +0000

    Update page.jsx

--- app/admin/products/page.jsx
+++ app/admin/products/page.jsx
@@ -301,16 +301,16 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  // PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
-import { getProducts } from "@/app/actions/product";
+// import {
+//   Pagination,
+//   PaginationContent,
+//   PaginationEllipsis,
+//   PaginationItem,
+//   PaginationLink,
+//   PaginationNext,
+//   PaginationPrevious,
+// } from "@/components/ui/pagination";
+// import { getProducts } from "@/app/actions/product";
 
 export default function ProductsPage() {
   // State for filters and pagination
