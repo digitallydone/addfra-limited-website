e687b8ddffe919dd3ed1ebfa1876d2a9505fea1e
Mansir Abdul Aziz <76412360+mansir93@users.noreply.github.com>
2025-06-24 13:55:57 +0000

    Update SearchForm.jsx

--- components/SearchForm.jsx
+++ components/SearchForm.jsx
@@ -15,12 +15,12 @@ export function SearchForm() {
 
   return (
     <form onSubmit={handleSearch} className="mb-8 flex justify-center gap-4">
-      <div className="relative w-full max-w-md">
+      <div className="relative w-full ma x-w-md">
         <Search className="absolute left-3 top-3 h-4 w-4 text-slate-500" />
         <Input
           name="make"
           placeholder="Search by make or model..."
-          className="pl-10"
+          className="pl-10 w-full"
         />
       </div>
       <Button type="submit">
